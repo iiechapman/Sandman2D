c42d7a6e91abdb1841c9a50149f56d2d4269f7f4
Evan <iiechapman@gmail.com>
2014-11-12 20:40:36 -0500

    Cleaned up code per google style guides, working on cleaning up code base before integrating lua

--- SDLGameEngine/systems/event/CollisionManager.cpp
+++ SDLGameEngine/systems/event/CollisionManager.cpp
@@ -1,117 +1,108 @@
-//
 //  CollisionManager.cpp
 //  SDLGameDevelopmentChapter1
-//
-//  Created by Evan Chapman on 7/8/14.
-//  Copyright (c) 2014 Evan Chapman. All rights reserved.
-//
+// Created by Evan Chapman on 7/8/14.
+// Copyright (c) 2014 Evan Chapman. All rights reserved.
 
 #include "CollisionManager.h"
 #include "Player.h"
 #include "Camera.h"
 #include "TileLayer.h"
+#include <vector>
 
-CollisionManager::CollisionManager(){
-    //New Collision manager
+CollisionManager::CollisionManager() {
+    // New Collision manager
 }
 
-CollisionManager::~CollisionManager(){
-    //Clean up collision manager
-    //updating
+CollisionManager::~CollisionManager() {
+    // Clean up collision manager
+    // updating
 }
 CollisionManager* CollisionManager::s_pInstance = 0;
 
-CollisionManager* CollisionManager::Instance(){
-    if (!s_pInstance){
+CollisionManager* CollisionManager::Instance() {
+    if (!s_pInstance) {
         s_pInstance = new CollisionManager();
     }
-    
     return s_pInstance;
 }
-
-//This function needs to be broken down into smaller pieces
+// This function needs to be broken down into smaller pieces
 void CollisionManager::checkPlayerTileCollision
-(Player* pPlayer, const vector<TileLayer*> &collisionLayers){
+(Player* pPlayer, const vector<TileLayer*> &collisionLayers) {
+    // Iterate through all collision layers
 
-    //Iterate through all collision layers
-    
     for (vector<TileLayer*>::const_iterator it = collisionLayers.begin();
-         it != collisionLayers.end() ; it++){
-        
-        //Grab current tilelayer
+         it != collisionLayers.end() ; it++) {
+        // Grab current tilelayer
         TileLayer* pTileLayer = (*it);
-        //Grab current tiles
+        // Grab current tiles
         vector<vector<int>> tiles = pTileLayer->getTileIDs();
-        
-        //Get layers position
+
+        // Get layers position
         Vector2D<double> layerPos = pTileLayer->getPosition();
 
-        //Calculate position on tile map
+        // Calculate position on tile map
         double x = 0, y = 0;
         x = ((layerPos.getX()) / pTileLayer->getTileSize());
         y = ((layerPos.getY()) / pTileLayer->getTileSize());
-    
-        
-        //If moving upwards or rightwards
-        double tileColumn = 0, tileRow = 0,tileID = 0;
+        // If moving upwards or rightwards
+        double tileColumn = 0, tileRow = 0, tileID = 0;
         if (pPlayer->GetParams().getVelocity().getX() > 0.0f ||
-            pPlayer->GetParams().getVelocity().getY() > 0.0f ){
-            
+            pPlayer->GetParams().getVelocity().getY() > 0.0f ) {
             tileColumn = ((((pPlayer->GetParams().getX()) +
                             Camera::Instance()->getPosition().getX() +
                            pPlayer->GetParams().getWidth()) /
                            pTileLayer->getTileSize()));
-            
+
             tileRow = (((pPlayer->GetParams().getY() +
                          Camera::Instance()->getPosition().getY() +
                         pPlayer->GetParams().getHeight()) /
                         pTileLayer->getTileSize()));
-            
-            //Check tile bounds
-            //cout << pTileLayer->getNumColumns() << "\n";
+
+            // Check tile bounds
+            // cout << pTileLayer->getNumColumns() << "\n";
             if ((tileRow + y )< pTileLayer->getNumRows() &&
-                (tileColumn + x) < pTileLayer->getNumColumns()){
+                (tileColumn + x) < pTileLayer->getNumColumns()) {
                 tileID = tiles[tileRow + y][tileColumn + x];
             } else {
                 tileID = 0;
             }
-        
-            //If moving downwards or leftwards
-            //Change to get params new method "Moving Down
+
+            // If moving downwards or leftwards
+            // Change to get params new method "Moving Down
         } else if (pPlayer->GetParams().getVelocity().getX() < 0.0f ||
-                   pPlayer->GetParams().getVelocity().getY() < 0.0f ){
-            //Get Current TileColum
+                   pPlayer->GetParams().getVelocity().getY() < 0.0f ) {
+            // Get Current TileColum
             tileColumn = ((pPlayer->GetParams().getX() +
                             Camera::Instance()->getPosition().getX())
                           / pTileLayer->getTileSize());
-            
+
             tileRow = ((pPlayer->GetParams().getY() +
                        Camera::Instance()->getPosition().getY())
                        / pTileLayer->getTileSize());
-            
-            //Check tile bounds
-            //cout << pTileLayer->getNumColumns() << "\n";
-            
+
+            // Check tile bounds
+            // cout << pTileLayer->getNumColumns() << "\n";
+
             if ((tileRow + y ) > 0 &&
-                (tileColumn + x) > 0){
+                (tileColumn + x) > 0) {
                 tileID = tiles[tileRow + y][tileColumn + x];
             } else {
                 tileID = 0;
             }
         }
-        //If tileID is not blank, collision occured
-        if (tileID != 0 ){
+        // If tileID is not blank, collision occured
+        if ( tileID != 0 ) {
             pPlayer->collision();
         }
     }
 }
 
 
-void CollisionManager::setCollisionLayers(vector<TileLayer*> collisionLayers){
+void CollisionManager::setCollisionLayers(vector<TileLayer*> collisionLayers) {
     m_pCollisionLayers = collisionLayers;
 }
 
-vector<TileLayer*>* CollisionManager::getCollisionLayers(){
+vector<TileLayer*>* CollisionManager::getCollisionLayers() {
     return &m_pCollisionLayers;
 }
 
@@ -161,4 +152,4 @@ vector<TileLayer*>* CollisionManager::getCollisionLayers(){
 
 
 
-//ENDs
+// END
